42d8accc4886a6c40236ffc0c0714371762638f3
rksvw <ritik.vw98@gmail.com>
2025-09-17 21:30:46 +0530

    setup database and integrated with orm prisma

---
+++ server/app/data/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const connectToDatabase = async () => {
+  try {
+    await pool.connect();
+    console.log("Connected to the database successfully.");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = { pool, connectToDatabase };
