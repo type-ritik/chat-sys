b7baba962ba137d1e1862b38e649fa3a42856d26
rksvw <ritik.vw98@gmail.com>
2025-10-02 17:18:33 +0530

    mv notify subs to App [response smoothely]

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,23 @@
+import { gql } from "@apollo/client";
+
 export const baseUrl = "http://localhost:4000/graphql";
+
+export const wsBaseUrl = "ws://localhost:4000/graphql";
 
 export const isValidEmail = (email: string) => {
   const regex = /^[a-zA-Z]{1,}[\d?\D]{1,}[@]{1}[a-z]{2,}.[com]{3}$/g;
   return regex.test(email);
 };
+
+export const NOTIFICATION_SUBSCRIPTION = gql`
+  subscription ($userId: String!) {
+    subNotify(userId: $userId) {
+      id
+      content
+      sender
+      isSeen
+      requestedId
+      timestamp
+    }
+  }
+`;
